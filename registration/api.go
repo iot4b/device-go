7df53c0871b098cebc75184adaa6a27ea5f56858
Andrey <free_apples@ymail.com>
2023-09-01 10:51:18 +0300

    fix(registration): fix select node

--- registration/api.go
+++ registration/api.go
@@ -4,12 +4,9 @@ import (
 	"device-go/helpers"
 	"device-go/shared/config"
 	"encoding/json"
-	"errors"
 	"github.com/coalalib/coalago"
 	log "github.com/ndmsystems/golog"
-	"io"
 	"math/rand"
-	"os"
 	"time"
 )
 
@@ -63,67 +60,39 @@ func NodeList(masterNode string) (list []string, err error) {
 // если есть файл контракта, то из него берет ноду.
 // если девайс долго не был в сети, то может быть такая ситуация, что нода на которой он висел уже не активна,
 // то повторяет процедуру регистрации
-func GetNode() (host string, needRegistration bool) {
-	contractFile, err := os.Open(config.Get("device.contractFile"))
+func GetNode() (host string) {
 	// если файл не найден, то получаем ноду с минимальным пингом
-	if err != nil && errors.Is(err, os.ErrNotExist) {
 
-		// get random from master nodes
-		masterNodeList := config.List("masterNodes")
+	// get random from master nodes
+	masterNodeList := config.List("masterNodes")
 
-		randomIndex := rand.Intn(len(masterNodeList))
-		masterNode := masterNodeList[randomIndex] + config.Get("coapServerPort")
+	randomIndex := rand.Intn(len(masterNodeList))
+	masterNode := masterNodeList[randomIndex] + config.Get("coapServerPort")
 
-		var list []string
-		err := helpers.RoundRobin(func() error {
-			var err error
-			list, err = NodeList(masterNode)
-			return err
-		}, 3*time.Second, 10)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// check min ping time to host
-		var lastTime time.Duration
-		fasterHost := masterNode
-		for _, host := range list {
-			t, err := ping(host + config.Get("coapServerPort"))
-			if err != nil {
-				log.Error(err)
-				continue
-			}
-			if lastTime > t || lastTime == 0 {
-				lastTime = t
-				fasterHost = host
-			}
-		}
-		host = fasterHost
-		needRegistration = true
-		return
-	}
-
-	defer contractFile.Close()
-
-	contract := map[string]interface{}{}
-	// иначе читаем ноду из файла контракта
-	data, err := io.ReadAll(contractFile)
+	var list []string
+	err := helpers.RoundRobin(func() error {
+		var err error
+		list, err = NodeList(masterNode)
+		return err
+	}, 3*time.Second, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = json.Unmarshal(data, &contract)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// пингуем ноду из контракта
-	host = contract["node"].(string)
-	if _, err := ping(host); err != nil {
-		// если нода не пингуется, то удаляем текущий контракт и новую ноду выбираем для девайса
-		err = os.Remove(config.Get("device.contractFile"))
+
+	// check min ping time to host
+	var lastTime time.Duration
+	fasterHost := masterNode
+	for _, host := range list {
+		t, err := ping(host + config.Get("coapServerPort"))
 		if err != nil {
 			log.Error(err)
+			continue
 		}
-		host, needRegistration = GetNode()
+		if lastTime > t || lastTime == 0 {
+			lastTime = t
+			fasterHost = host
+		}
 	}
+	host = fasterHost
 	return
 }
