5eaecaada476f0ed5326ee631ec9e96d16be7d97
Andrey <free_apples@ymail.com>
2023-09-05 14:20:27 +0300

    docs(registration): add detailed docs for endpointList

--- registration/helpers.go
+++ registration/helpers.go
@@ -8,6 +8,39 @@ import (
 	"time"
 )
 
+// endpointList - получаем список доступных нод для подключения
+// смарт-контракт электора хранит список доступных нод, с которыми можно установить соединение. при запросе на одну из
+// нод, можем получить полный список всех активных нод в системе
+//
+// выбранная нода в дальнейшем будет обслуживать девайс и устанавливать соединения с Client->Device
+//
+// в конфигах есть список мастер-нод, которые по-умолчанию задаются вместе с прошивкой девайса
+// они постоянные и не меняются
+//
+// todo сделать привязку к доменам, чтобы мастер ноды, вне зависимости от их ip, были всегда доступны для запроса
+// из masterNodeList выбирает случайным образом одну из мастер нод
+func endpointList(masterNodeList []string) (masterNode string, list []node, err error) {
+	// выбираем случайную ноду, чтобы одновременно на одну ноду не стучались все девайсы при инициализации,
+	// а было минимальное распределение
+	// перемешиваем список нод для случайного выбора
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(masterNodeList), func(i, j int) {
+		masterNodeList[i], masterNodeList[j] = masterNodeList[j], masterNodeList[i]
+	})
+
+	// пробегаемся по нодам и выбираем первую доступную
+	for _, masterNode = range masterNodeList {
+		// получаем с нее список нод
+		list, err = getEndpoints(masterNode)
+		if err == nil {
+			log.Debugf("masterNode: %s", masterNode)
+			return
+		}
+		log.Errorf("masterNode: %s, err: %s", masterNode, err.Error())
+	}
+	return
+}
+
 // ping - это получение info ноды,
 // duration - время, затраченное на выполнение запроса
 func ping(nodeHost string) (duration time.Duration, err error) {
@@ -41,30 +74,6 @@ func getEndpoints(node string) (list []node, err error) {
 	err = json.Unmarshal(resp.Body, &list)
 	if err != nil {
 		return nil, err
-	}
-	return
-}
-
-// endpointList - получаем список доступных нод для подключения
-// из masterNodeList выбирает случайным образом одну из мастер нод
-func endpointList(masterNodeList []string) (masterNode string, list []node, err error) {
-	// выбираем случайную ноду, чтобы одновременно на одну ноду не стучались все девайсы при инициализации,
-	// а было минимальное распределение
-	// перемешиваем список нод для случайного выбора
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(masterNodeList), func(i, j int) {
-		masterNodeList[i], masterNodeList[j] = masterNodeList[j], masterNodeList[i]
-	})
-
-	// пробегаемся по нодам и выбираем первую доступную
-	for _, masterNode = range masterNodeList {
-		// получаем с нее список нод
-		list, err = getEndpoints(masterNode)
-		if err == nil {
-			log.Debugf("masterNode: %s", masterNode)
-			return
-		}
-		log.Errorf("masterNode: %s, err: %s", masterNode, err.Error())
 	}
 	return
 }
