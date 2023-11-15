276cc52f92ad57d2f7633e6943de96fc6f301bb5
Alexey Spiridonov <forspall@gmail.com>
2023-11-15 17:46:10 +0700

    some fixes

--- dsm/cmd.go
+++ dsm/cmd.go
@@ -5,6 +5,8 @@ import (
 )
 
 type CMD struct {
+	UUID     string      `json:"uuid"`     // unique id
+	Ts       int64       `json:"ts"`       // timestamp
 	Sender   string      `json:"sender"`   // owner public key
 	Receiver EverAddress `json:"receiver"` // device contract address
 	Hash     string      `json:"hash"`     // hash all fields except Sign and Hash
@@ -12,9 +14,33 @@ type CMD struct {
 	Body     string      `json:"body"`     // command to execute
 }
 
+// Valid checks if all fields are filled
+func (c CMD) Valid() bool {
+	if len(c.UUID) == 0 {
+		return false
+	}
+	if c.Ts == 0 {
+		return false
+	}
+	if len(c.Sender) == 0 {
+		return false
+	}
+	if len(c.Receiver) == 0 {
+		return false
+	}
+	if len(c.Hash) == 0 {
+		return false
+	}
+	if len(c.Sign) == 0 {
+		return false
+	}
+	return true
+}
+
 // GetHash calculates hash sum of all fields except Sign and Hash
 func (c CMD) GetHash() string {
 	h := sha256.New()
-	h.Write([]byte(c.Sender + string(c.Receiver) + c.Body))
+	bt := []byte(string(c.Sender + string(c.Receiver) + c.Body + string(c.Ts) + string(c.UUID)))
+	h.Write(bt)
 	return string(h.Sum(nil))
 }
