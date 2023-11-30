20595b4d042c5c617dc26cacbbaec06737655ab6
Alexey Spiridonov <forspall@gmail.com>
2023-11-30 19:37:24 +0700

    doc

--- aliver/aliver.go
+++ aliver/aliver.go
@@ -8,6 +8,17 @@ import (
 	"github.com/pkg/errors"
 )
 
+/*
+
+1 - запускаем элайвер
+2 - если девайс прописался на ноде, то он шлет на нее элайв
+4- если нода элайв реджектит, то мы зааускаем процесс регистрации
+	если нрда не отвечает, то мы запускаем процесс регистрации
+	если нода отвечает, и не реджектит, то все ок
+
+
+*/
+
 func Run(s *coalago.Server, address string, nodeHost string, aliveInterval time.Duration) {
 	log.Info("run aliver")
 	for {
@@ -17,13 +28,15 @@ func Run(s *coalago.Server, address string, nodeHost string, aliveInterval time.
 		err := alive(s, nodeHost, address)
 		if err != nil {
 			log.Error(err)
+			//todo если не удалось отправить сообщение, то pзапускаем процесс переподключения, если накопилось 10 ошибок
+
 		}
 		//log.Infof("time: %dµs node: %s address: %s", time.Since(start).Nanoseconds(), nodeHost, address)
 	}
 }
 
 func alive(server *coalago.Server, nodeHost string, address string) error {
-	aliveMessage := coalago.NewCoAPMessage(coalago.CON, coalago.GET)
+	aliveMessage := coalago.NewCoAPMessage(coalago.ACK, coalago.GET)
 	aliveMessage.SetURIPath("/l")
 	aliveMessage.SetURIQuery("a", address)
 	return errors.Wrap(server.SendToSocket(aliveMessage, nodeHost), "sendToSocket")
