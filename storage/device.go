171b8ce3bdfe22595ccaa5a1e71b4ab1bf96712a
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2024-09-18 02:05:47 +0700

    add device name

--- storage/device.go
+++ storage/device.go
@@ -9,6 +9,7 @@ import (
 )
 
 type device struct {
+	Name    string          `json:"name,omitempty"`    //Device name
 	Address dsm.EverAddress `json:"address,omitempty"` //ever SC address текущего Device
 	Group   dsm.EverAddress `json:"group,omitempty"`   //ever SC address of DeviceGroup
 	Node    dsm.EverAddress `json:"node,omitempty"`    //ever SC address Node, с которой девайс создал последнее соединение
