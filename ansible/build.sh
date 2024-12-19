38e5668a74a928537b972cab8331e17df4e4f00a
AlexeySpiridonov <forspall@gmail.com>
2024-12-19 12:59:43 +0700

    feat(iot4b): add iot4b package with init script and configuration files

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o device.bin main.go
-ls -lh device.bin
+GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o package/iot4b/iot4b main.go
+ls -lh package/iot4b/iot4b
