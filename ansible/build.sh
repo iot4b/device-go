a48ed732572d5dc472bdb01dc097c49b8bd3ca0f
AlexeySpiridonov <forspall@gmail.com>
2024-12-19 15:38:11 +0700

    fix(iot4b): update build script and Makefile for correct binary and configuration paths

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o package/iot4b/iot4b main.go
+GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o package/iot4b/files/iot4b main.go
 ls -lh package/iot4b/iot4b
