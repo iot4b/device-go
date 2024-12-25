f7431dd2ebbc9bd19a6ac7260a4f6cf2bcae278d
AlexeySpiridonov <forspall@gmail.com>
2024-12-25 12:39:26 +0700

    feat(iot4b): update build script to create iot4b.ipk and add cleanup steps

--- ansible/build.sh
+++ ansible/build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o iot4b_package/opt/iot4b/iot4b main.go
-ls -lh iot4b_package/opt/iot4b
-cd iot4b_package
+cd iot4b_package 
+ls -lh opt/iot4b/iot4b | awk '{print $5}'
 tar -czvf control.tar.gz CONTROL
 tar -czvf data.tar.gz opt etc
 echo 2.0 > debian-binary
-tar r iot4b.ipk debian-binary control.tar.gz data.tar.gz
+tar -cf iot4b.ipk debian-binary control.tar.gz data.tar.gz
+echo "iot4b.ipk created"
+ls -lh iot4b.ipk | awk '{print $5}'
+echo "Cleaning up..."
+rm -f control.tar.gz
+rm -f data.tar.gz
+rm -f debian-binary
+rm -f opt/iot4b/iot4b
