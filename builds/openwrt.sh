9be6e02ca51970afa4255a405108a6fa1cc5680b
AlexeySpiridonov <forspall@gmail.com>
2025-01-21 14:28:31 +0700

    refactor(builds): update architecture and installed size in control file for iot4b_openwrt

--- builds/openwrt.sh
+++ builds/openwrt.sh
@@ -8,8 +8,8 @@ ls -lh opt/iot4b/iot4b | awk '{print $5}'
 tar -czvf control.tar.gz CONTROL
 tar -czvf data.tar.gz opt etc
 echo 2.0 > debian-binary
-#/opt/homebrew/opt/binutils/bin/gar r ../iot4b_openwrt.ipk debian-binary control.tar.gz data.tar.gz
-tar -czvf ../iot4b_openwrt.ipk debian-binary control.tar.gz data.tar.gz
+/opt/homebrew/opt/binutils/bin/gar r ../iot4b_openwrt.ipk debian-binary control.tar.gz data.tar.gz
+#tar -czvf ../iot4b_openwrt.ipk debian-binary control.tar.gz data.tar.gz
 echo "../iot4b_openwrt.ipk created"
 ls -lh ../iot4b_openwrt.ipk | awk '{print $5}'
 chmod +x ../iot4b_openwrt.ipk
