f43f6b94dc27399ed852b4cfb095a098e00a3855
AlexeySpiridonov <forspall@gmail.com>
2025-01-21 11:24:50 +0700

    refactor(builds): reorganize build scripts and configurations for iot4b and keenetic

---
+++ builds/keenetic.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o iot4b_keenetic/opt/iot4b/iot4b main.go
+
+cd builds/iot4b_keenetic
+rm -f ../iot4b_keenetic.ipk
+ls -lh opt/iot4b/iot4b | awk '{print $5}'
+tar -czvf control.tar.gz control postinst
+tar -czvf data.tar.gz opt
+echo 2.0 > debian-binary
+tar -czvf ../iot4b_keenetic.ipk debian-binary control.tar.gz data.tar.gz
+echo "../iot4b_keenetic.ipk created"
+ls -lh ../iot4b_keenetic.ipk | awk '{print $5}'
+chmod +x ../iot4b_keenetic.ipk
+echo "Cleaning up..."
+rm -f control.tar.gz
+rm -f data.tar.gz
+rm -f debian-binary
+rm -f opt/iot4b/iot4b
