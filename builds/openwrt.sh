65c5676cd655db197dff12f193e3e10f0918b133
AlexeySpiridonov <forspall@gmail.com>
2025-01-21 14:09:47 +0700

    refactor(builds): rename control file and update build script for iot4b_openwrt

--- builds/openwrt.sh
+++ builds/openwrt.sh
@@ -5,8 +5,8 @@ GOOS=linux GOARCH=mipsle go build -ldflags="-s -w" -o ./builds/iot4b_openwrt/opt
 cd builds/iot4b_openwrt
 rm -f ../iot4b_openwrt.ipk
 ls -lh opt/iot4b/iot4b | awk '{print $5}'
-tar -czvf control.tar.gz control postinst
-tar -czvf data.tar.gz opt
+tar -czvf control.tar.gz CONTROL
+tar -czvf data.tar.gz opt etc
 echo 2.0 > debian-binary
 #/opt/homebrew/opt/binutils/bin/gar r ../iot4b_openwrt.ipk debian-binary control.tar.gz data.tar.gz
 tar -czvf ../iot4b_openwrt.ipk debian-binary control.tar.gz data.tar.gz
