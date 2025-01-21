e745d1ef52b0bf9b51ab5acc722f947e059b9c51
AlexeySpiridonov <forspall@gmail.com>
2025-01-21 14:45:42 +0700

    refactor(builds): update license and improve packaging scripts for iot4b_openwrt

--- builds/keenetic.sh
+++ builds/keenetic.sh
@@ -6,7 +6,7 @@ cd builds/iot4b_keenetic
 rm -f ../iot4b_keenetic.ipk
 ls -lh opt/iot4b/iot4b | awk '{print $5}'
 tar -czvf control.tar.gz control postinst
-tar -czvf data.tar.gz opt
+tar -czvf data.tar.gz opt tmp
 echo 2.0 > debian-binary
 tar -czvf ../iot4b_keenetic.ipk debian-binary control.tar.gz data.tar.gz
 echo "../iot4b_keenetic.ipk created"
