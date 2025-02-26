8e71649449e7cbf1373f1926024c3e370d687d12
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 14:46:56 +0700

    update copy_packages.sh to add additional package copy commands for clarity

--- copy_packages.sh
+++ copy_packages.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+
+
 cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/mipsel/iot4b.ipk
+cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/mipsel-3.4_kn/iot4b.ipk
+
 
 cd /opt/iot4b/repo/mipsel
 opkg-make-index -a -p ./mipsel > Packages
 gzip -k Packages
 
 
-cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/mipsel-3.4_kn/iot4b.ipk
 cd /opt/iot4b/repo/mipsel-3.4_kn
 opkg-make-index -a -p ./mipsel-3.4_kn > Packages
 gzip -k Packages
