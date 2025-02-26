bd6b0f62d005700554dee8def85c72c7a23a8289
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 14:51:20 +0700

    update copy_packages.sh to rename package files for clarity and remove existing files before index creation

--- copy_packages.sh
+++ copy_packages.sh
@@ -2,15 +2,17 @@
 
 
 
-cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/mipsel/iot4b.ipk
-cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/mipsel-3.4_kn/iot4b.ipk
+cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/mipsel/iot4b-mipsel.ipk
+cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/mipsel-3.4_kn/iot4b-mipsel-3.4_kn.ipk
 
 
 cd /opt/iot4b/repo/mipsel
+rm -f *
 opkg-make-index -a -p ./mipsel > Packages
 gzip -k Packages
 
 
 cd /opt/iot4b/repo/mipsel-3.4_kn
+rm -f *
 opkg-make-index -a -p ./mipsel-3.4_kn > Packages
 gzip -k Packages
