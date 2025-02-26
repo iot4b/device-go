f4ff1155712560578ee65fd32641877e2d0d8328
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 14:01:35 +0700

    update copy_packages.sh to create package indexes for specific architectures

--- copy_packages.sh
+++ copy_packages.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-cp  ./builds/*.ipk /opt/iot4b/repo
+cp  ./builds/*openwrt.ipk /opt/iot4b/repo/mipsel
+
+cd /opt/iot4b/repo/mipsel
+opkg-make-index -a -p ./mipsel > Packages
+gzip -k Packages
+
+
+cp  ./builds/*openwrt.ipk /opt/iot4b/repo/mipsel-3.4_kn
+
+cd /opt/iot4b/repo/mipsel-3.4_kn
+opkg-make-index -a -p ./mipsel-3.4_kn > Packages
+gzip -k Packages
