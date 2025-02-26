cef85f0f77145e10b462d52a731ebd0c8a403876
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 15:18:51 +0700

    update copy_packages.sh to correct package index creation paths for mipsel and mipsel-3.4_kn directories

--- copy_packages.sh
+++ copy_packages.sh
@@ -8,10 +8,10 @@ cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/packages/mipsel/iot4b-mipsel.ipk
 cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/packages/mipsel-3.4_kn/iot4b-mipsel-3.4_kn.ipk
 
 cd /opt/iot4b/repo/packages/mipsel
-opkg-make-index -a -p ./ > Packages
+opkg-make-index -a ./ > ./Packages
 gzip -k Packages
 
 
 cd /opt/iot4b/repo/packages/mipsel-3.4_kn
-opkg-make-index -a -p ./ > Packages
+opkg-make-index -a ./ > ./Packages
 gzip -k Packages
