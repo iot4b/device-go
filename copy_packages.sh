c82d82b89c75707e6350bafed610b74c74a32614
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 15:29:22 +0700

    update copy_packages.sh to remove old packages, copy new ones, and create package indexes for mipsel and mipsel-3.4_kn directories

--- copy_packages.sh
+++ copy_packages.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 
+# Удаление старых пакетов
 rm -f /opt/iot4b/repo/packages/mipsel/*
 rm -f /opt/iot4b/repo/packages/mipsel-3.4_kn/*
 
+# Копирование новых пакетов
 cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/packages/mipsel/iot4b-mipsel.ipk
 cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/packages/mipsel-3.4_kn/iot4b-mipsel-3.4_kn.ipk
 
+# Создание индексов
 cd /opt/iot4b/repo/packages/mipsel
 opkg-make-index -a ./ > ./Packages
 gzip -k Packages
 
-
+# Создание индексов
 cd /opt/iot4b/repo/packages/mipsel-3.4_kn
 opkg-make-index -a ./ > ./Packages
 gzip -k Packages
