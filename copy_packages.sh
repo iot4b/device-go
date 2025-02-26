800cbfb7813178e0217e5207a1c8039adf127762
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 15:11:26 +0700

    update copy_packages.sh to adjust package index creation and gzip output for mipsel and mipsel-3.4_kn directories

--- copy_packages.sh
+++ copy_packages.sh
@@ -7,7 +7,11 @@ rm -f /opt/iot4b/repo/packages/mipsel-3.4_kn/*
 cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/packages/mipsel/iot4b-mipsel.ipk
 cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/packages/mipsel-3.4_kn/iot4b-mipsel-3.4_kn.ipk
 
+cd /opt/iot4b/repo/packages/mipsel
+opkg-make-index -a -p ./ > Packages
+gzip -k Packages
 
-cd /opt/iot4b/repo/
-opkg-make-index -a -p ./packages > Packages
+
+cd /opt/iot4b/repo/packages/mipsel-3.4_kn
+opkg-make-index -a -p ./ > Packages
 gzip -k Packages
