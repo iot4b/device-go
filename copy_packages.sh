00c41e4adf3ae888f1da0d0c190d2c390e2d4448
AlexeySpiridonov <forspall@gmail.com>
2025-02-26 14:04:16 +0700

    update copy_packages.sh to specify package filenames for better clarity

--- copy_packages.sh
+++ copy_packages.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-cp  ./builds/*openwrt.ipk /opt/iot4b/repo/mipsel
+cp  ./builds/iot4b_openwrt.ipk /opt/iot4b/repo/mipsel/iot4b.ipk
 
 cd /opt/iot4b/repo/mipsel
 opkg-make-index -a -p ./mipsel > Packages
 gzip -k Packages
 
 
-cp  ./builds/*openwrt.ipk /opt/iot4b/repo/mipsel-3.4_kn
-
+cp  ./builds/iot4b_keenetic.ipk /opt/iot4b/repo/mipsel-3.4_kn/iot4b.ipk
 cd /opt/iot4b/repo/mipsel-3.4_kn
 opkg-make-index -a -p ./mipsel-3.4_kn > Packages
 gzip -k Packages
