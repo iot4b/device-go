e070f1082bc6e4406a90bf1f40f03069df85c331
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2025-09-09 13:16:58 +0700

    update formula sha256

--- Formula/iot4b-device.rb
+++ Formula/iot4b-device.rb
@@ -2,7 +2,7 @@ class Iot4bDevice < Formula
   desc "IOT4B Device"
   homepage "https://github.com/iot4b/device-go"
   url "https://github.com/iot4b/device-go/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "897229a83ca74cdb8a32f529968a217b94fe88c5978de871c732b400d21c4571"
+  sha256 "b1305ce01260403b20833855e92d41ccc3e47744fdcc7726111394d4ab52e149"
   license "MIT"
 
   depends_on "go" => :build
