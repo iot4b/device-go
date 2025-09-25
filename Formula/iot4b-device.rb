b2d7500eba80e8947ac89aa3b5405cf0370400ee
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2025-09-25 12:30:53 +0700

    update brew formula

--- Formula/iot4b-device.rb
+++ Formula/iot4b-device.rb
@@ -1,8 +1,8 @@
 class Iot4bDevice < Formula
   desc "IOT4B Device"
   homepage "https://github.com/iot4b/device-go"
-  url "https://github.com/iot4b/device-go/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "5141e376f0d1faa078ab1e3930c2ab7df924318b41c3e900f191bec00c4ed5bd"
+  url "https://github.com/iot4b/device-go/archive/refs/tags/1.2.0.tar.gz"
+  sha256 "089cb2776e400515e84be576f5210206f46b80465b4387dfbe01c5292585b0f8"
   license "MIT"
 
   depends_on "go" => :build
@@ -13,6 +13,14 @@ class Iot4bDevice < Formula
 
     # Install default config
     (etc/"iot4b-device").install "config/prod.yml"
+  end
+
+  service do
+    run [opt_bin/"iot4b-device"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/iot4b-device.log"
+    error_log_path var/"log/iot4b-device.log"
   end
 
   test do
