912fed8d2313b7c9d19673f3fe3b45f1c1f778f0
Alexander Mitrikovsky <mitrikovsky@gmail.com>
2025-10-21 10:08:46 +0700

    brew formula 1.2.5

--- Formula/iot4bd.rb
+++ Formula/iot4bd.rb
@@ -1,8 +1,8 @@
 class Iot4bd < Formula
   desc "IOT4B Device"
   homepage "https://github.com/iot4b/device-go"
-  url "https://github.com/iot4b/device-go/archive/refs/tags/1.2.4.tar.gz"
-  sha256 "e818cae34147808309f9468ea26372363aae6baa2fbc0539e6bedae2f2abce0b"
+  url "https://github.com/iot4b/device-go/archive/refs/tags/1.2.5.tar.gz"
+  sha256 "640418038f2d901fdd00f72c7778be382d61b5e31bcaeff439d32a45b184094b"
   license "MIT"
 
   depends_on "go" => :build
@@ -24,6 +24,6 @@ class Iot4bd < Formula
   end
 
   test do
-    assert_match "iot4bd version 1.2.4", shell_output("#{bin}/iot4bd --version")
+    assert_match "iot4bd version 1.2.5", shell_output("#{bin}/iot4bd --version")
   end
 end
